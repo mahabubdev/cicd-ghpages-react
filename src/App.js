0569a25ff835db318b0370e311288403487ee781
Md Mahabub <mahabub200bd@gmail.com>
2021-02-26 14:22:20 +0600

    first up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="react-logo" />
+        <h1 style={{ marginBottom: 16 }}>Create-React-App</h1>
+        
+
+        <Router basename="https://mahabubdev.github.io/cicd-ghpages-react">
+          <div style={{
+            margin: '1rem 0',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '.5rem 0'
+          }}>
+            <Link to='/'>Page_One</Link>
+            <Link to='/two'>Page_Two</Link>
+            <Link to='/h37b3ru'>Page_404</Link>
+          </div>
+
+          <Switch>
+            <Route path={'/'} component={pageOne} exact />
+            <Route path={'/two'} component={pageTwo} exact />
+            <Route path={'/*'} component={notFound} exact />
+          </Switch>
+        </Router>
+      </div>
     </div>
+  );
+}
+
+// https://mahabubdev.github.io/cicd-ghpages-react
+
+// components
+const pageOne = () => {
+  return (
+    <>
+      <h2>Page no. 1</h2>
+    </>
+  );
+}
+
+const pageTwo = () => {
+  return (
+    <>
+      <h2>Page no. 2</h2>
+    </>
+  );
+}
+
+const notFound = () => {
+  return (
+    <>
+      <h2 style={ { color: 'red' } }>Page not found! 404 Error.</h2>
+    </>
   );
 }
 
